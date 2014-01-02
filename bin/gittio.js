8839347a86127e2596080a0446864c6c3993b73a
Fokke Zandbergen <mail@fokkezb.nl>
2014-01-02 14:25:11 +0100

    reorganized a bit

--- bin/gittio.js
+++ bin/gittio.js
@@ -1,184 +1,50 @@
 #!/usr/bin/env node
 
-var os = require('os'),
-	path = require('path'),
-	request = require('request'),
-	ghdownload = require('github-download'),
-	_ = require('underscore'),
-	fs = require('fs-extra'),
-	AdmZip = require('adm-zip'),
-	ProgressBar = require('progress');
+var gittio = require('../lib/gittio'),
+	program = require('commander'),
+	package = require('../package.json');
 
-var bar = new ProgressBar(':bar', {
-	total: 10
-});
+program
+	.version(package.version, '-v, --version')
+	.description(package.about.name)
+	.usage('command <args> [options]')
+	.option('-g, --global', 'install modules under global path');
 
-// TODO: Check if in project root
+program.command('install')
+	.description('install all missing modules and widgets')
+	.action(install);
 
-if (process.argv.length > 2) {
-	install(process.argv[2], process.argv[3]);
+program.command('install <id>')
+	.description('install the latest version of a module or widget');
 
-} else {
-	findWidgets();
+program.command('install <id>@<version>')
+	.description('install a specific version of a module or widget');
+
+program.parse(process.argv);
+
+if (program.args.length === 0 || typeof program.args[program.args.length - 1] === 'string') {
+    program.help();
 }
 
-function findWidgets() {
-	var alloyConfigPath = path.join(process.cwd(), 'app', 'config.json');
+function install() {
 
-	if (fs.existsSync(alloyConfigPath)) {
+	if (typeof this.args[0] === 'string') {
+		var input = this.args[0];
+		var at = input.indexOf('@');
+		var id, version;
 
-		fs.readJson(alloyConfigPath, function(err, data) {
+		if (at > 0) {
+			id = input.substr(0, at);
+			version = input.substr(at + 1);
 
-			if (err) {
-				console.error(err);
-				process.exit(0);
-			}
+			return gittio.install(id, version);
 
-			console.log(data);
+		} else {
+			id = input;
+			return gittio.install(id);
+		}
 
-			if (data.dependencies) {
-
-				_.each(data.dependencies, function(version, widget) {
-					install('widget', widget);
-				});
-			}
-		});
+	} else {
+		return gittio.install();
 	}
-}
-
-function findModules() {
-	var alloyConfigPath = path.join(process.cwd(), 'app', 'config.json');
-
-	if (fs.existsSync(alloyConfigPath)) {
-
-		fs.readJson(alloyConfigPath, function(err, data) {
-
-			if (err) {
-				console.error(err);
-				process.exit(0);
-			}
-
-			console.log(data);
-
-			if (data.dependencies) {
-
-				_.each(data.dependencies, function(version, widget) {
-					install('widget', widget);
-				});
-			}
-		});
-	}
-}
-
-function install(type, uid) {
-	var url = 'http://registry.gitt.io/' + type + '/' + uid;
-
-	request(url, function(error, response, body) {
-
-		if (!error && response.statusCode == 200) {
-
-			info = JSON.parse(body);
-
-			console.log(info);
-
-			download({
-				user: info.user,
-				repo: info.repo,
-				branch: info.branch
-			}, function(err, tmpPath) {
-
-				if (err) {
-					console.error(err);
-					process.exit(0);
-				}
-
-				console.log(tmpPath);
-
-				var trgPath = path.join(process.cwd(), 'app', 'widgets', uid);
-
-				fs.mkdirs(trgPath, function(err) {
-
-					if (err) {
-						console.error(err);
-						process.exit(0);
-					}
-
-					var plfPath = _.first(_.values(info.platforms));
-
-					var srcPath = path.join(tmpPath, plfPath);
-
-					fs.copy(srcPath, trgPath, function(err) {
-
-						if (err) {
-							console.error(err);
-							process.exit(0);
-						}
-
-						console.log('done');
-						process.exit(0);
-					});
-				});
-			});
-		}
-	});
-}
-
-function generateTempDir() {
-	return path.join(os.tmpDir(), Date.now().toString() + '-' + Math.random().toString().substring(2));
-}
-
-function download(_opts, _cb) {
-	var tmpdir = generateTempDir(),
-		zipBaseDir = _opts.repo + '-' + _opts.branch,
-		zipFile = path.join(tmpdir, zipBaseDir + '.zip');
-
-	var zipUrl = "https://nodeload.github.com/" + _opts.user + "/" + _opts.repo + "/zip/" + _opts.branch;
-
-	fs.mkdir(tmpdir, function(err) {
-
-		if (err) {
-			return _cb(err);
-		}
-
-		request.get(zipUrl).pipe(fs.createWriteStream(zipFile)).on('close', function() {
-
-			extract(zipFile, tmpdir, function(err) {
-
-				if (err) {
-					return _cb(err);
-				}
-
-				_cb(null, path.join(tmpdir, zipBaseDir));
-			});
-		});
-	});
-}
-
-function extract(zipFile, outputDir, _cb) {
-	var zip = new AdmZip(zipFile),
-		entries = zip.getEntries(),
-		pending = entries.length;
-
-	function checkDone(err) {
-
-		if (err) {
-			_cb(err);
-		}
-
-		pending -= 1;
-
-		if (pending === 0) {
-			_cb();
-		}
-	}
-
-	entries.forEach(function(entry) {
-
-		if (entry.isDirectory) {
-			return checkDone();
-		}
-
-		var file = path.resolve(outputDir, entry.entryName);
-		fs.outputFile(file, entry.getData(), checkDone);
-	});
 }
