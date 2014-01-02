60d753383e6b8403cc651dc055790f384d82d0f5
David Bankier <dbankier@hotmail.com>
2014-01-03 10:43:49 +1100

    reformatted whitespace: tabs --> 2 spaces

--- bin/gittio.js
+++ bin/gittio.js
@@ -1,52 +1,52 @@
 #!/usr/bin/env node
 
 var gittio = require('../lib/gittio'),
-		config = require('../lib/config'),
-	program = require('commander'),
-	package = require('../package.json');
+    config = require('../lib/config'),
+    program = require('commander'),
+    package = require('../package.json');
 
 program
-	.version(package.version, '-v, --version')
-	.description(package.about.name)
-	.usage('command <args> [options]')
-	.option('-g, --global', 'install modules under global path')
-	.option('-f, --force', 'install components even if already found');
+  .version(package.version, '-v, --version')
+  .description(package.about.name)
+  .usage('command <args> [options]')
+  .option('-g, --global', 'install modules under global path')
+  .option('-f, --force', 'install components even if already found');
 
 program.command('install')
-	.description('install all missing modules and widgets')
-	.action(install);
+  .description('install all missing modules and widgets')
+  .action(install);
 
 program.command('install <id>') // ony here for help
-	.description('install the latest version of a module or widget');
+  .description('install the latest version of a module or widget');
 
 program.command('install <id>@<version>') // ony here for help
-	.description('install a specific version of a module or widget');
+  .description('install a specific version of a module or widget');
 
 program.parse(process.argv);
 
 if (program.args.length === 0 || typeof program.args[program.args.length - 1] === 'string') {
-		program.help();
+    program.help();
 }
 
 function install(env) {
-	var args = this.args;
-	var params = {
-		force: this.force,
-		global: this.global
-	}
-	config.init(function() {
-		if (typeof args[0] === 'string') {
-			var input = args[0];
-			var at = input.indexOf('@');
-			var id, version;
+  var args = this.args;
+  var params = {
+    force: this.force,
+    global: this.global
+  }
+  config.init(function() {
+    if (typeof args[0] === 'string') {
+      var input = args[0];
+      var at = input.indexOf('@');
+      var id, version;
 
-			if (at > 0) {
-				params.id = input.substr(0, at);
-				params.version = input.substr(at + 1);
-			} else {
-				params.id = input;
-			}
-		}
-		return gittio.install(params);
-	});
+      if (at > 0) {
+        params.id = input.substr(0, at);
+        params.version = input.substr(at + 1);
+      } else {
+        params.id = input;
+      }
+    }
+    return gittio.install(params);
+  });
 }
