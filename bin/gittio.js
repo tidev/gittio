537d41c563bae3585f53f01f7d9c818758d67b81
Fokke Zandbergen <mail@fokkezb.nl>
2014-01-07 15:29:17 +0100

    pretty JSON for info, added 'uninstall' closing #2

--- bin/gittio.js
+++ bin/gittio.js
@@ -1,44 +1,55 @@
 #!/usr/bin/env node
 
 var gittio = require('../lib/gittio'),
-    config = require('../lib/config'),
-    program = require('commander'),
-    package = require('../package.json');
+  config = require('../lib/config'),
+  program = require('commander'),
+  package = require('../package.json');
 
 program
   .version(package.version, '-v, --version')
   .description(package.about.name)
   .usage('command <args> [options]')
-  .option('-g, --global', 'install modules under global path')
-  .option('-f, --force', 'install components even if already found');
+  .option('-g, --global', 'use global path for modules')
+  .option('-f, --force', 'install components even if already found')
+  .option('-p, --platform', 'apply to a specific platform only');
 
 program.command('install')
   .description('install all missing modules and widgets')
   .action(install);
 
-program.command('install <id>') // ony here for help
+// ony here for help
+program.command('install <id>')
   .description('install the latest version of a module or widget');
 
-program.command('install <id>@<version>') // ony here for help
+// ony here for help
+program.command('install <id>@<version>')
   .description('install a specific version of a module or widget');
+
+program.command('uninstall <id>')
+  .description('uninstall a module or widget')
+  .action(uninstall);
+
+// ony here for help
+program.command('uninstall <id>@<version>')
+  .description('uninstall a specific version of a module or widget');
 
 program.command('info <id>')
   .description('display info about a component')
   .action(gittio.info);
 
-
 program.parse(process.argv);
 
 if (program.args.length === 0 || typeof program.args[program.args.length - 1] === 'string') {
-    program.help();
+  program.help();
 }
 
 function install(env) {
   var args = this.args;
   var params = {
     force: this.force,
-    global: this.global
-  }
+    global: this.global,
+    platform: this.platform
+  };
   config.init(function() {
     if (typeof args[0] === 'string') {
       var input = args[0];
@@ -53,5 +64,29 @@ function install(env) {
       }
     }
     return gittio.install(params);
+  });
+}
+
+function uninstall(env) {
+  var args = this.args;
+  var params = {
+    force: this.force,
+    global: this.global,
+    platform: this.platform
+  };
+  config.init(function() {
+    if (typeof args[0] === 'string') {
+      var input = args[0];
+      var at = input.indexOf('@');
+      var id, version;
+
+      if (at > 0) {
+        params.id = input.substr(0, at);
+        params.version = input.substr(at + 1);
+      } else {
+        params.id = input;
+      }
+    }
+    return gittio.uninstall(params);
   });
 }
